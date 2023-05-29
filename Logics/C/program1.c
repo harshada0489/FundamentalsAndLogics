b3979cd4243a0f2615152f34ea48afc35163bb66
harshada0489 <72671266+harshada0489@users.noreply.github.com>
2023-05-29 16:30:29 -0700

    Add files via upload

---
+++ Logics/C/program1.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Jay Ganesh...\n");
+
+    return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+//command to compile the code
+//gcc program1.c -o Myexe
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+//gcc           GNU C Compiler(Name of C Compiler)
+//program1.c    Name of file which contains the code
+//-o            Option to generate the output
+// Myexe        Name of exectrable file that we want to create
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+// Command to execute the code
+//Myexe         For windoes users
+//./Myexe       For Linux or MacOS users
+
+////////////////////////////////////////////////////////////////////////////////////
+
+//Basic commands
+//1: cd         Change Directory(Windiws/Linux/MacOS)
+//2: dir        List all the names of files(Windows)
+//2: ls         List all the names of files(Linux/MacOS)
+//4: clear      Clear the terminal window(Linux/MacOS)
+//4: cls        Clear the terminal window(Windows)
+
+/////////////////////////////////////////////////////////////////////////////////////
